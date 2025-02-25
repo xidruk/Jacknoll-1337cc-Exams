65844685a9316a5ab9421f2d3e2fe9d8c5f12bf9
Xidruk <xidruk@gmail.com>
2025-02-25 07:52:29 -0700

    Update epur_str.c

--- jknd-exam-02-LVL-03/epur_str.c
+++ jknd-exam-02-LVL-03/epur_str.c
@@ -1,29 +1,34 @@
-#include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
-int is_a_valid_separator(char c)
+int is_valid_spl(char c)
 {
-    return (c == ' ' || c == '\t');
+    return (c == ' ');
 }
 
-void display_only_one_space(char *data)
+void display_epur_str(char *data)
 {
     if (!data)
     {
         write(1, "\n", 1);
         return ;
     }
+    while (is_valid_spl(*data))
+    {
+        data++;
+    }
+    
     while (*data)
     {
-        if (!is_a_valid_separator(*data))
+        if (!is_valid_spl(*data))
         {
-            write(1, data, 1);
+            write (1, data, 1);
             data++;
         }
         else
         {
-            while (*data && is_a_valid_separator(*data))
+            while (*data && is_valid_spl(*data))
             {
                 data++;
             }
@@ -36,14 +41,13 @@ void display_only_one_space(char *data)
     write(1, "\n", 1);
 }
 
-
 int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        write(1, "\n", 1);
+        write (1, "\n", 1);
         return (1);
     }
-    display_only_one_space(argv[1]);
+    display_epur_str(argv[1]);
     return (0);
 }
