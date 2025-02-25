1e99e2fb9981a8b86a6c73251d87a8fbf602a844
Xidruk <xidruk@gmail.com>
2025-02-25 08:34:21 -0700

    Update expand_str.c

--- jknd-exam-02-LVL-03/expand_str.c
+++ jknd-exam-02-LVL-03/expand_str.c
@@ -2,7 +2,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int is_a_valid_separator(char c)
+
+int is_valid_spl(char c)
 {
     return (c == ' ' || c == '\t');
 }
@@ -14,35 +15,39 @@ void expand_string(char *data)
         write(1, "\n", 1);
         return ;
     }
+    while (*data && is_valid_spl(*data))
+    {
+        data++;
+    }
     while (*data)
     {
-        if (is_a_valid_separator(*data))
+        if (!is_valid_spl(*data))
         {
-            while (*data && is_a_valid_separator(*data))
+            write(1, data, 1);
+            data++;
+        }
+        else
+        {
+            while (*data && is_valid_spl(*data))
             {
                 data++;
             }
             if (*data)
             {
-                write(1, "   ", 3);
+                write (1, "   ", 3);
             }
         }
-        if (*data && !is_a_valid_separator(*data))
-        {
-            write(1, data, 1);
-            data++;
-        }
     }
+    write (1, "\n", 1);
 }
 
 int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        write(1, "\n", 1);
+        write(1, "'\n", 1);
         return (1);
     }
     expand_string(argv[1]);
-    write(1, "\n", 1);
     return (0);
 }
